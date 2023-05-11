6ec32029b5a5ba9ba396bd7af60f69f3f1c09e53
Carlos Junior <99406137+Cjr88@users.noreply.github.com>
2023-05-10 22:09:22 -0300

    Add files via upload

---
+++ exercicio.ts
@@ -0,0 +1,24 @@
+let comprimento: number = 19;
+let largura: number = 44;
+
+function multiplica(comprimento: number, largura: number): number{
+    return comprimento * largura;
+}
+
+let saudacao1: string = 'Ola'
+let nome1: string = 'carlos'
+
+
+function dizOi(saudacao1: string, nome1:string): string{
+    return saudacao1 + nome1;
+}
+console.log(dizOi)
+
+// type pessoa = {
+//     saudacao: string;
+//     nome: string;
+// }
+
+// function exibePessoa(pessoa: pessoa) {
+//     console.log(pessoa.nome)
+// }
